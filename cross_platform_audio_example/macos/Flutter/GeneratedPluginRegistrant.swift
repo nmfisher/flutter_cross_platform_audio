f1c27a2f89fa6dcc64ddebe4fbf4e85c7f721327
Nick Fisher <nick.fisher@avinium.com>
2024-10-31 17:00:42 +0800

    update example project ios/macos project files

--- cross_platform_audio_example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cross_platform_audio_example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import audio_session
 import audioplayers_darwin
+import cross_platform_audio_native
 import just_audio
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  CrossPlatformAudioNativePlugin.register(with: registry.registrar(forPlugin: "CrossPlatformAudioNativePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
