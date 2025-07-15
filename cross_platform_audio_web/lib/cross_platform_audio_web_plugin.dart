ff55016579c0f2f4fee1093fe4e8e40c3a04b61f
Nick Fisher <nick.fisher@avinium.com>
2025-07-15 13:17:54 +0800

    fix web streaming playback

--- cross_platform_audio_web/lib/cross_platform_audio_web_plugin.dart
+++ cross_platform_audio_web/lib/cross_platform_audio_web_plugin.dart
@@ -21,7 +21,7 @@ class WebAudioService extends AudioService {
   }
 
   @override
-  Future play(String path,
+  Future<CancelPlayback> play(String path,
       {AudioSource source = AudioSource.File,
       String? package,
       void Function()? onBegin,
@@ -44,11 +44,14 @@ class WebAudioService extends AudioService {
     if (onComplete != null) {
       bufferSource.onended = onComplete.toJS;
     }
+    return () async {
+      bufferSource.stop();
+    };
   }
 
   @override
-  Future<void Function()> playBuffer(Uint8List data,
-      {AudioEncoding encoding = AudioEncoding.PCM16,
+  Future<CancelPlayback> playBuffer(Uint8List data,
+      {AudioEncoding encoding = const PCM16(),
       void Function()? onBegin,
       void Function()? onComplete,
       int? sampleRate,
@@ -57,7 +60,7 @@ class WebAudioService extends AudioService {
     try {
       var bufferSource = w.AudioBufferSourceNode(context);
       late w.AudioBuffer audioBuffer;
-      if (encoding == AudioEncoding.PCM16) {
+      if (encoding == const PCM16()) {
         if (stereo == true) {
           throw Exception("TODO");
         }
@@ -73,7 +76,7 @@ class WebAudioService extends AudioService {
             length: data.length ~/ 2);
         audioBuffer = w.AudioBuffer(options);
         audioBuffer.copyToChannel(jsData, 0);
-      } else if (encoding == AudioEncoding.PCMF32) {
+      } else if (encoding == PCMF32()) {
         var f32Data = data.buffer.asFloat32List(data.offsetInBytes);
         var jsData = f32Data.toJS;
         var options = w.AudioBufferOptions(
@@ -97,7 +100,7 @@ class WebAudioService extends AudioService {
 
       bufferSource.start(0, start ?? 0);
 
-      return () {
+      return () async {
         bufferSource.stop();
       };
     } catch (err, st) {
@@ -137,12 +140,83 @@ class WebAudioService extends AudioService {
   }
 
   @override
-  Future playStream(Stream<Uint8List> data, int frequency, bool stereo,
-      {void Function()? onComplete}) {
-    // TODO: implement playStream
-    throw UnimplementedError();
+  Future<CancelPlayback> playStream(
+      Stream<Uint8List> data, int frequency, bool stereo,
+      {void Function()? onComplete}) async {
+    try {
+      // Create a ScriptProcessorNode for real-time audio processing
+      // Note: ScriptProcessorNode is deprecated but still widely supported
+      // In production, consider using AudioWorklet when Flutter web supports it better
+      final int bufferSize = 4096; // Common buffer size
+      final int channels = stereo ? 2 : 1;
+
+      final scriptNode = context.createScriptProcessor(bufferSize, 0, channels);
+
+      // Buffer to store incoming audio data
+      final List<double> audioQueue = [];
+      bool isStreamComplete = false;
+
+      // Listen to the stream and convert data to audio samples
+      late StreamSubscription<Uint8List> streamSubscription;
+      streamSubscription = data.listen(
+        (Uint8List chunk) {
+          // Convert Uint8List to Float32 samples (assuming PCM16 format)
+          final samples =
+              chunk.buffer.asInt16List().map((sample) => sample / 32768.0);
+          audioQueue.addAll(samples);
+        },
+        onDone: () {
+          isStreamComplete = true;
+          streamSubscription.cancel();
+        },
+        onError: (error) {
+          print('Stream error: $error');
+          streamSubscription.cancel();
+          onComplete?.call();
+        },
+      );
+
+      // Set up the audio processing callback
+      scriptNode.onaudioprocess = ((w.AudioProcessingEvent event) {
+        final outputBuffer = event.outputBuffer;
+        final int framesToProcess = outputBuffer.length;
+
+        for (int channel = 0; channel < channels; channel++) {
+          final channelData = Float32List(framesToProcess);
+
+          // Fill the channel data from our queue
+          for (int i = 0; i < framesToProcess; i++) {
+            if (audioQueue.isNotEmpty) {
+              channelData[i] = audioQueue.removeAt(0);
+            } else {
+              channelData[i] = 0.0; // Silence if no data available
+            }
+          }
+
+          // Copy data to the output buffer
+          outputBuffer.copyToChannel(channelData.toJS, channel);
+        }
+
+        // Check if stream is complete and queue is empty
+        if (isStreamComplete && audioQueue.isEmpty) {
+          scriptNode.disconnect();
+          onComplete?.call();
+        }
+      }).toJS;
+
+      // Connect the script processor to the audio context destination
+      scriptNode.connect(context.destination);
+
+      return () async {
+        scriptNode.disconnect();
+      };
+    } catch (err, st) {
+      print('Error in playStream: $err');
+      print('Stack trace: $st');
+      throw Exception("Error playing stream: $err\n$st");
+    }
   }
-  
+
   @override
   Future<Duration> getDuration(Uint8List data, AudioEncoding encoding) {
     // TODO: implement getDuration
